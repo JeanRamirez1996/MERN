98e346905802d16de1481bb3624645e7b01bffec
JEAN PIERRE RAMIREZ <jeanramz@hotmail.com>
2022-01-29 16:10:54 -0500

    first commit

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component{
+        render(){ 
+        return(
+            <h1>Hello Dojo!</h1>
+        );
+    }
+}
+
+export default ToDo;
